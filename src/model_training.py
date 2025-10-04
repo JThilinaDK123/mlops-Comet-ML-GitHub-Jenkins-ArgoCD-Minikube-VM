fc6f14108d0a236958d77981e38abf3e86567c1a
Thilina Jayasinghe <jthilinadk123@gmail.com>
2025-10-04 21:43:25 +0530

    first commit

---
+++ src/model_training.py
@@ -0,0 +1,182 @@
+import os
+import pandas as pd
+import comet_ml
+import joblib
+from sklearn.model_selection import RandomizedSearchCV
+import lightgbm as lgb
+from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
+from src.logger import get_logger
+from src.custom_exception import CustomException
+from config.paths_config import *
+from config.model_params import *
+from utils.common_functions import read_yaml,load_data
+
+logger = get_logger(__name__)
+
+class ModelTraining:
+
+    """
+        Read all the inputs that needs to run the 
+        model training part
+    """
+
+    def __init__(self,train_path,test_path,model_output_path):
+        self.train_path = train_path
+        self.test_path = test_path
+        self.model_output_path = model_output_path
+
+        self.params_dist = LIGHTGM_PARAMS
+        self.random_search_params = RANDOM_SEARCH_PARAMS
+
+        self.experiment = comet_ml.Experiment(
+            api_key = "HWuSeY42O7eDv1ZxpclqIszAO",
+            project_name = "mlops-project-01",
+            workspace = "jthilinadk123"
+        )
+
+    def load_and_split_data(self):
+
+        """
+            Load the training and test datasets
+        """
+
+        try:
+            logger.info(f"Loading data from {self.train_path}")
+            train_df = load_data(self.train_path)
+
+            logger.info(f"Loading data from {self.test_path}")
+            test_df = load_data(self.test_path)
+
+            X_train = train_df.drop(columns=["booking_status"])
+            y_train = train_df["booking_status"]
+
+            X_test = test_df.drop(columns=["booking_status"])
+            y_test = test_df["booking_status"]
+
+            logger.info("Data splitted sucefully for Model Training")
+
+            return X_train,y_train,X_test,y_test
+        except Exception as e:
+            logger.error(f"Error while loading data {e}")
+            raise CustomException("Failed to load data" ,  e)
+        
+
+    def train_lgbm(self,X_train,y_train):
+
+        """
+            Train the LGB model
+        """
+
+        try:
+            logger.info("Intializing our model")
+
+            lgbm_model = lgb.LGBMClassifier(random_state=self.random_search_params["random_state"])
+
+            logger.info("Starting our Hyperparamter tuning")
+
+            random_search = RandomizedSearchCV(
+                estimator=lgbm_model,
+                param_distributions=self.params_dist,
+                n_iter = self.random_search_params["n_iter"],
+                cv = self.random_search_params["cv"],
+                n_jobs=self.random_search_params["n_jobs"],
+                verbose=self.random_search_params["verbose"],
+                random_state=self.random_search_params["random_state"],
+                scoring=self.random_search_params["scoring"]
+            )
+
+            logger.info("Starting our Hyperparamter tuning")
+
+            random_search.fit(X_train,y_train)
+
+            logger.info("Hyperparamter tuning completed")
+
+            best_params = random_search.best_params_
+            best_lgbm_model = random_search.best_estimator_
+
+            logger.info(f"Best paramters are : {best_params}")
+
+            return best_lgbm_model
+        
+        except Exception as e:
+            logger.error(f"Error while training model {e}")
+            raise CustomException("Failed to train model" ,  e)
+    
+    def evaluate_model(self , model , X_test , y_test):
+
+        """
+            Evaluate the Model
+        """
+
+        try:
+            logger.info("Evaluating our model")
+
+            y_pred = model.predict(X_test)
+
+            accuracy = accuracy_score(y_test,y_pred)
+            precision = precision_score(y_test,y_pred)
+            recall = recall_score(y_test,y_pred)
+            f1 = f1_score(y_test,y_pred)
+
+            self.experiment.log_metric("Accuracy Score", accuracy)
+            self.experiment.log_metric("Precision", precision)
+            self.experiment.log_metric("Recall", recall)
+            self.experiment.log_metric("F1 Score", f1)
+
+            return {
+                "accuracy" : accuracy,
+                "precison" : precision,
+                "recall" : recall,
+                "f1" : f1
+            }
+        
+        except Exception as e:
+            logger.error(f"Error while evaluating model {e}")
+            raise CustomException("Failed to evaluate model" ,  e)
+        
+    def save_model(self,model):
+
+        """
+            Save the Model
+        """
+
+        try:
+            os.makedirs(os.path.dirname(self.model_output_path),exist_ok=True)
+
+            logger.info("saving the model")
+            joblib.dump(model , self.model_output_path)
+            self.experiment.log_asset(MODEL_OUTPUT_PATH)
+            logger.info(f"Model saved to {self.model_output_path}")
+
+        except Exception as e:
+            logger.error(f"Error while saving model {e}")
+            raise CustomException("Failed to save model" ,  e)
+    
+    def run(self):
+
+        """
+            Read all the functions in a one def function
+            Additionaly, start doing the experiment tracking with mlflow
+        """
+         
+        try:
+            logger.info("Starting our Model Training pipeline")
+            X_train,y_train,X_test,y_test = self.load_and_split_data()
+            best_lgbm_model = self.train_lgbm(X_train,y_train)
+            metrics = self.evaluate_model(best_lgbm_model ,X_test , y_test)
+            self.save_model(best_lgbm_model)
+
+            logger.info("Model Training sucesfullly completed")
+
+        except Exception as e:
+            logger.error(f"Error in model training pipeline {e}")
+            raise CustomException("Failed during model training pipeline" ,  e)
+        
+if __name__=="__main__":
+    trainer = ModelTraining(PROCESSED_TRAIN_FILE_PATH, PROCESSED_TEST_FILE_PATH, MODEL_OUTPUT_PATH)
+    trainer.run()
+        
+
+    
+
+            
